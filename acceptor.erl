22a4bdd4dcabdf534af5559dd56e371c2bc67abe
George Masgras <gmasgras@gmail.com>
2023-11-21 11:57:52 +0100

    some work on the acceptor

--- acceptor.erl
+++ acceptor.erl
@@ -3,9 +3,9 @@
 
 start(Name, PanelId) ->
   spawn(fun() -> init(Name, PanelId) end).
-        
+
 init(Name, PanelId) ->
-  Promised = order:null(), 
+  Promised = order:null(),
   Voted = order:null(),
   Value = na,
   acceptor(Name, Promised, Voted, Value, PanelId).
@@ -13,35 +13,36 @@ init(Name, PanelId) ->
 acceptor(Name, Promised, Voted, Value, PanelId) ->
   receive
     {prepare, Proposer, Round} ->
-      case order:gr(..., ...) of
+      case order:gr({Round, Proposer}, {Promised}) of
         true ->
-          ... ! {promise, ..., ..., ...},               
+          Proposer ! {promise, {Round, Proposer}, Voted, Value},
       io:format("[Acceptor ~w] Phase 1: promised ~w voted ~w colour ~w~n",
-                 [Name, ..., ..., Value]),
+                 [Name, {Round, Proposer}, Voted, Value]),
           % Update gui
           Colour = case Value of na -> {0,0,0}; _ -> Value end,
-          PanelId ! {updateAcc, "Voted: " ++ io_lib:format("~p", [Voted]), 
-                     "Promised: " ++ io_lib:format("~p", [...]), Colour},
-          acceptor(Name, ..., Voted, Value, PanelId);
+          PanelId ! {updateAcc, "Voted: " ++ io_lib:format("~p", [Voted]),
+                     "Promised: " ++ io_lib:format("~p", [{Round, Proposer}]), Colour},
+          acceptor(Name, {Round, Proposer}, Voted, Value, PanelId);
         false ->
-          ... ! {sorry, {prepare, ...}},
-          acceptor(Name, ..., Voted, Value, PanelId)
+          Proposer ! {sorry, {prepare, {Round, Proposer}}},
+          acceptor(Name, Promised, Voted, Value, PanelId)
       end;
     {accept, Proposer, Round, Proposal} ->
-      case order:goe(..., ...) of
+      case order:goe({Round, Proposer}, Promised) of
         true ->
-          ... ! {vote, ...},
-          case order:goe(..., ...) of
+          Proposer ! {vote, {Round, Proposer}},
+          % check if vote is > previous vote
+          case order:goe(vote, Voted) of
             true ->
       io:format("[Acceptor ~w] Phase 2: promised ~w voted ~w colour ~w~n",
                  [Name, Promised, ..., ...]),
               % Update gui
-              PanelId ! {updateAcc, "Voted: " ++ io_lib:format("~p", [...]), 
+              PanelId ! {updateAcc, "Voted: " ++ io_lib:format("~p", [...]),
                          "Promised: " ++ io_lib:format("~p", [Promised]), ...},
               acceptor(Name, Promised, ..., ..., PanelId);
             false ->
               acceptor(Name, Promised, ..., ..., PanelId)
-          end;                            
+          end;
         false ->
           ... ! {sorry, {accept, ...}},
           acceptor(Name, Promised, Voted, Value, PanelId)
