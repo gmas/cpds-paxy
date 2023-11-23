4536ce61d0727f35f3d75c3cf7cb99aeeab5fbbb
George Masgras <gmasgras@gmail.com>
2023-11-23 11:13:06 +0100

    Acceptor fixes
    - only send Round to Proposer
    - implement accept case

--- acceptor.erl
+++ acceptor.erl
@@ -15,7 +15,7 @@ acceptor(Name, Promised, Voted, Value, PanelId) ->
     {prepare, Proposer, Round} ->
       case order:gr({Round, Proposer}, {Promised}) of
         true ->
-          Proposer ! {promise, {Round, Proposer}, Voted, Value},
+          Proposer ! {promise, Round, Voted, Value},
       io:format("[Acceptor ~w] Phase 1: promised ~w voted ~w colour ~w~n",
                  [Name, {Round, Proposer}, Voted, Value]),
           % Update gui
@@ -24,27 +24,27 @@ acceptor(Name, Promised, Voted, Value, PanelId) ->
                      "Promised: " ++ io_lib:format("~p", [{Round, Proposer}]), Colour},
           acceptor(Name, {Round, Proposer}, Voted, Value, PanelId);
         false ->
-          Proposer ! {sorry, {prepare, {Round, Proposer}}},
+          Proposer ! {sorry, {prepare, Round}},
           acceptor(Name, Promised, Voted, Value, PanelId)
       end;
     {accept, Proposer, Round, Proposal} ->
       case order:goe({Round, Proposer}, Promised) of
         true ->
           Proposer ! {vote, {Round, Proposer}},
-          % check if vote is > previous vote
-          case order:goe(vote, Voted) of
+          % check if vote round > max Voted round
+          case order:goe({Round, Proposer}, Voted) of
             true ->
-      io:format("[Acceptor ~w] Phase 2: promised ~w voted ~w colour ~w~n",
-                 [Name, Promised, ..., ...]),
+              io:format("[Acceptor ~w] Phase 2: promised ~w voted ~w colour ~w~n",
+                 [Name, Promised, {Round, Proposer}, Proposal]),
               % Update gui
-              PanelId ! {updateAcc, "Voted: " ++ io_lib:format("~p", [...]),
-                         "Promised: " ++ io_lib:format("~p", [Promised]), ...},
-              acceptor(Name, Promised, ..., ..., PanelId);
+              PanelId ! {updateAcc, "Voted: " ++ io_lib:format("~p", [{Round, Proposer}]),
+                         "Promised: " ++ io_lib:format("~p", [Promised]), Proposal},
+              acceptor(Name, Promised, {Round, Proposer}, Proposal, PanelId);
             false ->
-              acceptor(Name, Promised, ..., ..., PanelId)
+              acceptor(Name, Promised, Voted, Value, PanelId)
           end;
         false ->
-          ... ! {sorry, {accept, ...}},
+          Proposer ! {sorry, {accept, Round}},
           acceptor(Name, Promised, Voted, Value, PanelId)
       end;
     stop ->
